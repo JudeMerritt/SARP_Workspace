073e0ae548ab541f0c312200d03eb62b06b139b4
Jude Merritt <jm@Judes-MacBook-Air-502.local>
2026-01-10 00:28:58 -0800

    fixed led.c

--- myWork/led.c
+++ myWork/led.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include "include/mmio.h"
+#include "myWork/systick.h"
 
 typedef enum {
     GREEN,
@@ -8,20 +9,27 @@ typedef enum {
 }led_t;
 
 typedef struct {
-    field32_t rcc_field;  // Which clock to enable
     uint8_t port;         // Which port (GPIOB = 1, GPIOE = 4)
     uint8_t pin;          // Which pin
 } led_config_t;
 
 static const led_config_t LED_MAP[] = {
-    [GREEN]    = {RCC_AHB4ENR_GPIOBEN, 1, 0},
-    [YELLOW]  = {RCC_AHB4ENR_GPIOBEN, 4, 1},
-    [RED] = {RCC_AHB4ENR_GPIOEEN, 1, 14}
+    [GREEN]  = {1, 0},
+    [YELLOW] = {4, 1},
+    [RED]    = {1, 14}
 };
 
 void led_init(int led) {
     // Set bus clock
-    SET_FIELD(RCC_AHB1ENR, LED_MAP[led].rcc_field);
+    switch (led) {
+        case (GREEN):
+        case (RED):
+            SET_FIELD(RCC_AHB4ENR, RCC_AHB4ENR_GPIOBEN);
+            break;
+        case (YELLOW):
+            SET_FIELD(RCC_AHB4ENR, RCC_AHB4ENR_GPIOEEN);
+            break;
+    }
 
     // Set pin mode to output
     WRITE_FIELD(GPIOx_MODER[LED_MAP[led].port], GPIOx_MODER_MODEx[LED_MAP[led].pin], 0b01);
